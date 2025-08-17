7b342843471db59b58f39a6d90eda2544f0585ac
@mauricio-jacki <mauriciojacki@gmail.com>
2025-08-17 00:46:48 -0300

    se borraron datos del template

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import Game from "./scenes/Game.js";
 
-// Create a new Phaser config object
 const config = {
   type: Phaser.AUTO,
   width: 720,
@@ -24,11 +23,7 @@ const config = {
       debug: true,
     },
   },
-  // List of scenes to load
-  // Only the first scene will be shown
-  // Remember to import the scene before adding it to the list
   scene: [Game],
 };
 
-// Create a new Phaser game instance
 window.game = new Phaser.Game(config);
